644dc2021ec1d0d69860d56f8bbbd314c5a6ff94
Kshitij Pandey <kshitijpandey32@gmail.com>
2024-12-10 21:48:40 +1100

    make vite config input dynamic

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,47 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 import injectHTML from "vite-plugin-html-inject";
-import { TailwindCSSVitePlugin } from "tailwindcss-vite-plugin";
-import tailwind from "tailwindcss";
-import autoprefixer from "autoprefixer";
 import path from "path";
+import fs from "fs";
 
 const root = resolve(__dirname, "src");
 const outDir = resolve(__dirname, "dist");
+
+// function to collect all HTML files recursively
+function collectHtmlFiles(rootDir: string) {
+	let htmlFiles: { [key: string]: string } = {};
+
+	// walk through directories and collect HTML files
+	function walkDirectory(dir: string) {
+		const files = fs.readdirSync(dir);
+
+		files.forEach((file) => {
+			const filePath = path.join(dir, file);
+			const stat = fs.statSync(filePath);
+
+			if (stat.isDirectory()) {
+				walkDirectory(filePath);
+			} else if (filePath.endsWith(".html")) {
+				const relativePath = path.relative(rootDir, filePath);
+				// remove the '.html' extension from the key
+				let key = relativePath.replace(/\.html$/, "");
+				// handle the case where the file is 'index.html' in any subdirectory
+				if (key.endsWith("/index")) {
+					// extract the last folder name
+					const parts = key.split("/");
+					key = parts.length > 2 ? parts[parts.length - 2] : parts[0]; // use the last folder name
+				}
+				htmlFiles[key] = resolve(rootDir, relativePath);
+			}
+		});
+	}
+
+	walkDirectory(rootDir);
+
+	return htmlFiles;
+}
+
+const articles = collectHtmlFiles(root);
 
 export default defineConfig({
 	appType: "mpa",
@@ -29,22 +63,8 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 				main: resolve(root, "index.html"),
-				"mod-synth": resolve(root, "/mod-synth/index.html"),
-				nada: resolve(root, "/nada/index.html"),
-				"music-theory": resolve(root, "/nada/music-theory/index.html"),
-				"main-scale": resolve(root, "/nada/main-scale/index.html"),
-				"code-tech": resolve(root, "/tech/index.html"),
-				"10-principles": resolve(root, "/tech/principles/index.html"),
-				"null-type": resolve(root, "/tech/null-type/index.html"),
-				oscillator: resolve(root, "/mod-synth/oscillator/index.html"),
-				amplifier: resolve(root, "/mod-synth/amplifier/index.html"),
-				lfo: resolve(root, "/mod-synth/lfo/index.html"),
-				filter: resolve(root, "/mod-synth/filter/index.html"),
-				aside: resolve(root, "/aside/index.html"),
-				"history-of-sound": resolve(root, "/aside/history-of-sound.html"),
-				timbre: resolve(root, "/aside/timbre/index.html"),
 				"404": resolve(root, "404.html"),
-				spandrel: resolve(root, "/aside/spandrel/index.html"),
+				...articles,
 			},
 		},
 	},
